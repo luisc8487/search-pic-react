54490915690122342ce3a2f9bf8824e697dee850
L C <LF9904@gmail.com>
2025-05-07 23:45:21 -0400

    added

--- pics/src/api.jsx
+++ pics/src/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: "Client-ID sdfdgfdgsdfgfdshgsf dgqdshabdlj,
+      Authorization: "Client-ID sdfdgfdgsdfgfdshgsf dgqdshabdlj",
     },
     params: {
       query: term,
