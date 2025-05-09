a5d0966bc21cf7aede933bf90c2a48edd88441c3
L C <LF9904@gmail.com>
2025-05-09 00:04:07 -0400

    added ways

--- pics/src/api.jsx
+++ pics/src/api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
+require("dotenv").config();
 
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: "Client-ID sdfdgfdgsdfgfdshgsf dgqdshabdlj",
+      Authorization: `Client-ID ${process.env.API_KEY}`,
     },
     params: {
       query: term,
